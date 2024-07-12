12b98e2bd5dc46d74d5d8c4177b23c951ee68879
Tushar Nagar <nagartushar771@gmail.com>
2024-07-12 13:48:52 +0530

    frontend init

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="">Tushar Nagar</div>;
+};
+
+export default App;
--- client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div>DonateMe Dapp</div>;
-};
-
-export default App;
